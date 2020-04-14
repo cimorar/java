151be9f05d9beda89e3d5671c186052d482c4f79
Radim <radimx@gmail.com>
2020-04-14 18:59:30 +0200

    27/3 - CZUStudent rucne

---
+++ CZUTest/CZUTest-web/target/generated-sources/annotations/cz/raci/entities/CzuStudent_.java
@@ -0,0 +1,16 @@
+package cz.raci.entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-04-14T18:39:30")
+@StaticMetamodel(CzuStudent.class)
+public class CzuStudent_ { 
+
+    public static volatile SingularAttribute<CzuStudent, String> surname;
+    public static volatile SingularAttribute<CzuStudent, String> name;
+    public static volatile SingularAttribute<CzuStudent, String> dateOfBirth;
+    public static volatile SingularAttribute<CzuStudent, Long> id;
+
+}
