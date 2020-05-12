d18bb8d3eaf26d3306d4003f3591e2e6067c1af8
Radim <radimx@gmail.com>
2020-05-13 00:12:17 +0200

    session 23/4 - changes to console CustomerClient

--- PizzaRestClient/src/main/java/cz/racimora/pizzarestclient/runner/Main.java
+++ PizzaRestClient/src/main/java/cz/racimora/pizzarestclient/runner/Main.java
@@ -12,6 +12,7 @@ import cz.racimora.pizzarestclient.entities.Customers;
 import cz.racimora.pizzarestclient.entities.Pizza;
 import cz.racimora.pizzarestclient.entities.PizzaOrder;
 import java.util.ArrayList;
+import javax.ws.rs.ClientErrorException;
 
 /**
  *
@@ -24,10 +25,7 @@ public class Main {
         restClient.addCustomer();
         System.out.println("Pocet klientu: " + restClient.countREST());
         
-        Customers customers = restClient.findAllCustomers_XML(Customers.class);
-        for (Customer customer : customers.getCustomers()) {
-            System.out.println("id: " + customer.getId() +  ", name: " + customer.getName() + ", surname: " + customer.getSurname());
-        }
+        printAllCustomers(restClient);
 
         // create a newCustomer object and initialize it
         Customer newCustomer  = new Customer();
@@ -49,5 +47,22 @@ public class Main {
         
         // call REST API to create a new customer and all cascaded objects
         restClient.create_JSON(newCustomer);
+        
+        // edit existing customer
+        String customerId = "5";
+        Customer existingCustomer = restClient.find_JSON(Customer.class, customerId);
+        existingCustomer.setSurname("Rajska");
+        restClient.edit_JSON(existingCustomer, customerId);
+        
+        printAllCustomers(restClient);
+    }
+
+    private static void printAllCustomers(CustomerClient restClient) throws ClientErrorException {
+        System.out.println("********** Start of customer list ************");
+        Customers customers = restClient.findAllCustomers_XML(Customers.class);
+        for (Customer customer : customers.getCustomers()) {
+            System.out.println("id: " + customer.getId() +  ", name: " + customer.getName() + ", surname: " + customer.getSurname());
+        }
+        System.out.println("********** End of customer list ************");
     }
 }
